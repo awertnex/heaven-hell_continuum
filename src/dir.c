0425c153906f97273d509e1859687982f6d71cf9
lilya <lily.awertnex@gmail.com>
2025-03-29 21:05:15 +0300

    Finish building instance directory structure

--- src/dir.c
+++ src/dir.c
@@ -3,7 +3,6 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <dirent.h>
-#include <defines.h>
 
 #include "h/main.h"
 #include "h/dir.h"
@@ -12,26 +11,28 @@
 
 str mc_c_grandpath[PATH_MAX] = {0};
 str mc_c_subpath[PATH_MAX] = {0};
-str instance_directory_structure[16][NAME_MAX] = {0};
-str world_directory_structure[4][NAME_MAX] = {0};
+str mc_c_launcher_path[PATH_MAX] = {0};
+str instance_directory_structure[17][NAME_MAX] = {0};
+str world_directory_structure[3][NAME_MAX] = {0};
 
 // ---- functions --------------------------------------------------------------
 void init_grandpath_directory()
 {
+    snprintf(mc_c_grandpath, strlen(getenv("HOME")) + 14, "%s/minecraft.c/", getenv("HOME"));
     if (!mkdir(mc_c_grandpath, 0775))
-        LOGINFO("'minecraft.c' Directory Created at 'HOME/minecraft.c'");
+        LOGINFO("'minecraft.c' Directory Created: 'HOME/minecraft.c/'");
     else
-        LOGINFO("'minecraft.c' Directory Exists at 'HOME/minecraft.c'");
+        LOGINFO("'minecraft.c' Directory Exists at 'HOME/minecraft.c/'");
 }
 
 FILE *instance;
 FILE *info;
 void init_instance_directory(str *instance_name)
 {
+    str string[PATH_MAX];
     if ((info = fopen("src/info/dir.txt", "r")))
     {
         LOGINFO("Loading Instance Directory Structure");
-        str string[PATH_MAX];
         for (u16 i = 0, j = 0, stage = 0; i < 0xFFF && fgets(string, PATH_MAX, info); ++i, ++j)
         {
             if (!strncmp(string, "\n", 1))
@@ -70,23 +71,43 @@ void init_instance_directory(str *instance_name)
     }
     else
     {
-        LOGFATAL("File 'src/info/instance_directory_structure.txt' Not Found, Aborting Instance Creation");
+        LOGFATAL("File Not Found 'src/info/instance_directory_structure.txt', Instance Creation Aborted");
         state &= ~STATE_ACTIVE;
         return;
     }
 
     snprintf(mc_c_subpath, strlen(mc_c_grandpath) + strlen(instance_name), "%s%s", mc_c_grandpath, instance_name);
-
     if (mkdir(mc_c_subpath, 0775))
     {
-        instance = fopen(instance_name, "r+");
-        LOGINFO("Instance Opened: %s", instance_name);
+        memset(string, 0, PATH_MAX);
+        for (u8 i = 0; i < 255 && string[0]; ++i)
+        {
+            snprintf(string,
+                    strlen(mc_c_subpath) + strlen(instance_directory_structure[i]),
+                    "%s%s", mc_c_subpath, instance_directory_structure[i]);
+
+            mkdir(string, 0775);
+            LOGINFO("Directory Created '%s/%s'", instance_name, instance_directory_structure[i]);
+        }
     }
     else
     {
-        instance = fopen(instance_name, "r+");
-        LOGINFO("Instance Created: %s", instance_name);
+        LOGFATAL("Path Invalid '%s'", mc_c_subpath);
+        state &= ~STATE_ACTIVE;
+        return;
     }
 
-    if (instance != 0) fclose(instance);
+    // TODO: make the launcher before executing these steps
+    instance = fopen(mc_c_launcher_path, "r+");
+    if (instance)
+    {
+        LOGINFO("Instance Opened '%s'", instance_name);
+        fclose(instance);
+    }
+    else LOGINFO("Instance Created '%s'", instance_name);
+}
+
+void init_world_directory()
+{
+    //TODO: init world directory
 }
--- src/h/dir.h
+++ src/h/dir.h
@@ -9,9 +9,17 @@ enum DirectoryStructure
     MC_C_DIR_SAVES = 1,
     MC_C_DIR_RESOURCES = 2,
     MC_C_DIR_TEXTURES = 3,
-    MC_C_DIR_SOUNDS = 4,
-    MC_C_DIR_INFO = 5,
-    MC_C_DIR_SCREENSHOTS = 6,
+    MC_C_DIR_BLOCK = 4,
+    MC_C_DIR_ENVIRONMENT = 5,
+    MC_C_DIR_ENTITY = 6,
+    MC_C_DIR_FONT = 7,
+    MC_C_DIR_GUI = 8,
+    MC_C_DIR_ITEM = 9,
+    MC_C_DIR_MISC = 10,
+    MC_C_DIR_PAINTING = 11,
+    MC_C_DIR_SOUNDS = 12,
+    MC_C_DIR_INFO = 13,
+    MC_C_DIR_SCREENSHOTS = 14,
 
     MC_C_DIR_ADVANCEMENTS = 0,
     MC_C_DIR_CHUNKS = 1,
@@ -21,8 +29,14 @@ enum DirectoryStructure
 // ---- declarations -----------------------------------------------------------
 extern str mc_c_grandpath[PATH_MAX];
 extern str mc_c_subpath[PATH_MAX];
-extern str instance_directory_structure[16][NAME_MAX];
-extern str world_directory_structure[4][NAME_MAX];
+extern str mc_c_launcher_path[PATH_MAX];
+extern str instance_directory_structure[17][NAME_MAX];
+extern str world_directory_structure[3][NAME_MAX];
+
+// ---- signatures -------------------------------------------------------------
+void init_grandpath_directory();
+void init_instance_directory(str *instance_name);
+void init_world_directory();
 
 #define DIR_H
 #endif
