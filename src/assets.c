4547cdf3ecd654acee613e439be12c8d652858f2
lilya <lily.awertnex@gmail.com>
2025-03-29 21:03:44 +0300

    Update to match codebase

--- src/assets.c
+++ src/assets.c
@@ -5,7 +5,11 @@ _section_items =================================================================
 _section_tools =================================================================
 */
 
+#include <stdio.h>
+#include <string.h>
+
 #include "h/assets.h"
+#include "h/dir.h"
 
 // ---- variables --------------------------------------------------------------
 u16 base_texture_size;
@@ -15,9 +19,19 @@ texture_layout ThreeSide;
 texture_layout ThreeSideAlt;
 texture_layout FourSide;
 
-Texture2D texture_block_grass;
-Texture2D texture_block_cobblestone;
-Texture2D texture_block_cobbled_deepslate;
+Texture2D textures[3] = {0};
+enum TextureNames
+{
+    TEXTURE_BLOCK_GRASS = 0,
+    TEXTURE_BLOCK_COBBLESTONE = 1,
+    TEXTURE_BLOCK_COBBLED_DEEPSLATE = 2,
+}; /* TextureNames */
+str block_names[3][NAME_MAX] =
+{
+    "grass_block_side.png",
+    "cobblestone.png",
+    "cobbled_deepslate.png",
+};
 
 // ---- functions --------------------------------------------------------------
 void init_texture_layouts()
@@ -32,28 +46,34 @@ void init_texture_layouts()
 void init_textures()
 {
     base_texture_size = 16;
-    texture_block_grass =               LoadTexture(DIR_TEXTURES_BLOCKS"grass_block_side.png");
-    texture_block_cobblestone =         LoadTexture(DIR_TEXTURES_BLOCKS"cobblestone.png");
-    texture_block_cobbled_deepslate =   LoadTexture(DIR_TEXTURES_BLOCKS"cobbled_deepslate.png");
+
+    str string[PATH_MAX + NAME_MAX] = {0};
+    for (u8 i = 0; i < 3; ++i)
+    {
+        snprintf(string,
+                strlen(mc_c_subpath) + strlen(instance_directory_structure[MC_C_DIR_BLOCK]) + strlen(block_names[i]),
+                "%s%s%s",
+                mc_c_subpath, instance_directory_structure[MC_C_DIR_BLOCK], block_names[i]);
+
+        textures[i] = LoadTexture(string);
+    }
 }
 
 void unload_textures()
 {
-    UnloadTexture(texture_block_grass);
-    UnloadTexture(texture_block_cobblestone);
-    UnloadTexture(texture_block_cobbled_deepslate);
+    for (u8 i = 0; i < 3; ++i)
+        UnloadTexture(textures[i]);
 }
 
 // =============================================================================
 // _section_blocks =============================================================
 // =============================================================================
-
 block BlockGrass = 
 {
     Grass,
     BLOCK_STATE_SOLID,
     &ThreeSide,
-    &texture_block_grass,
+    &textures[TEXTURE_BLOCK_GRASS],
 }; /* Block Grass */
 
 block BlockCobbleStone =
@@ -61,7 +81,7 @@ block BlockCobbleStone =
     CobbleStone,
     BLOCK_STATE_SOLID,
     &OneSide,
-    &texture_block_cobblestone,
+    &textures[TEXTURE_BLOCK_COBBLESTONE],
 }; /* Block Cobblestone */
 
 block BlockCobbledDeepslate =
@@ -69,7 +89,7 @@ block BlockCobbledDeepslate =
     CobbledDeepSlate,
     BLOCK_STATE_SOLID,
     &OneSide,
-    &texture_block_cobbled_deepslate,
+    &textures[TEXTURE_BLOCK_COBBLED_DEEPSLATE],
 }; /* Block Cobbled Deepslate */
 
 // =============================================================================
