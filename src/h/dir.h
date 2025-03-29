1a41b87a38f57c37a2300f31be95853a5e368175
lilya <lily.awertnex@gmail.com>
2025-03-29 18:08:49 +0300

    Transfer definitions: dir.c -> dir.h

--- src/h/dir.h
+++ src/h/dir.h
@@ -1,6 +1,28 @@
 #ifndef DIR_H
 
-#define DIR_TEXTURES_BLOCKS "resources/textures/blocks/"
+#include <limits.h>
+#include <defines.h>
+
+enum DirectoryStructure
+{
+    MC_C_DIR_BIN = 0,
+    MC_C_DIR_SAVES = 1,
+    MC_C_DIR_RESOURCES = 2,
+    MC_C_DIR_TEXTURES = 3,
+    MC_C_DIR_SOUNDS = 4,
+    MC_C_DIR_INFO = 5,
+    MC_C_DIR_SCREENSHOTS = 6,
+
+    MC_C_DIR_ADVANCEMENTS = 0,
+    MC_C_DIR_CHUNKS = 1,
+    MC_C_DIR_ENTITIES = 2,
+}; /* DirectoryStructure */
+
+// ---- declarations -----------------------------------------------------------
+extern str mc_c_grandpath[PATH_MAX];
+extern str mc_c_subpath[PATH_MAX];
+extern str instance_directory_structure[16][NAME_MAX];
+extern str world_directory_structure[4][NAME_MAX];
 
 #define DIR_H
 #endif
