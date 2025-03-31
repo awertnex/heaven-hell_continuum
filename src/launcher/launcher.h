823e81b676a25fe6f3b0ab5131eb5b9cb14eafbf
lilya <lily.awertnex@gmail.com>
2025-03-31 14:57:09 +0300

    Separate launcher code into its own program and directory

--- src/launcher/launcher.h
+++ src/launcher/launcher.h
@@ -1,10 +1,12 @@
 #ifndef LAUNCHER_H
 
-#include "main.h"
-#include "gui.h"
-#include "logic.h"
+#define VECTOR2_TYPES
+#include "../h/defines.h"
 
-#define LAUNCHER_ENABLED 1
+#define MC_C_LAUNCHER_VERSION "Launcher 0.1.0"
+#define color(r, g, b, v, a) (Color){((f32)r/255)*v, ((f32)g/255)*v, ((f32)b/255)*v, a}
+
+// ---- colors -----------------------------------------------------------------
 #define COL_LAUNCHER_BG color(0x30, 0x30, 0x30, 0xFF, 0xFF)
 
 // ---- declarations -----------------------------------------------------------
