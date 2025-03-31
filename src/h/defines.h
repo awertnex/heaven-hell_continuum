e449fc1118ab492afd8e2b0698a4ef16cb29b5af
lilya <lily.awertnex@gmail.com>
2025-03-31 08:41:41 +0300

    remove 'true/false' redefines

--- src/h/defines.h
+++ src/h/defines.h
@@ -4,9 +4,6 @@
 #include <stdbool.h>
 
 // ---- data types -------------------------------------------------------------
-#define true 1
-#define false 0
-
 typedef uint8_t		u8;
 typedef uint16_t	u16;
 typedef uint32_t	u32;
