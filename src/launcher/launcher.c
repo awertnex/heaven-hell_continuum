03600b6458b56fe70306060417addf7f252af707
lilya <lily.awertnex@gmail.com>
2025-03-31 19:24:12 +0300

    Launching launcher successful

--- src/launcher/launcher.c
+++ src/launcher/launcher.c
@@ -1,54 +1,49 @@
 #include <raylib.h>
 
-#include "h/launcher.h"
-#include "h/keymaps.h"
-#include "h/logger.h"
+#include "launcher.h"
+#include "../h/logger.h"
 
+u8 state = 0;
 v2i16 render_size = {500, 720};
 
 void init_launcher()
 {
-    state |= STATE_LAUNCHER;
+    state |= STATE_ACTIVE;
     SetWindowState(FLAG_MSAA_4X_HINT);
     InitWindow(render_size.x, render_size.y, "minecraft.c Launcher");
-    SetWindowState(FLAG_WINDOW_UNDECORATED);
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
 
     if (LOGGING_DEBUG)
         SetWindowState(FLAG_WINDOW_TOPMOST);
 }
 
+int main(void) // ---- main ----------------------------------------------------
+{
+    init_launcher();
+
+    while (state & STATE_ACTIVE)
+    {
+        update_launcher_input();
+        update_launcher();
+    }
+
+    CloseWindow();
+    return 0;
+}
+
 void update_launcher()
 {
+    if (IsWindowResized())
+        render_size = (v2i16){GetRenderWidth(), GetRenderHeight()};
+
     BeginDrawing();
-    ClearBackground(COL_LAUNCHER_BG);
-
-    draw_text_centered(font_regular,
-            MC_C_VERSION,
-            (v2i16){(f32)render_size.x/2, 20},
-            font_size, 3, COL_TEXT_DEFAULT, 1);
-
-    draw_button(texture_hud_widgets, button,
-            (v2i16){render_size.x/2, render_size.y/2},
-            BTN_DONE,
-            &btn_func_quit,
-            "Quit");
+    ClearBackground(COL_BG);
 
     EndDrawing();
 }
 
-u8 close_launcher()
-{
-    ClearWindowState(FLAG_WINDOW_UNDECORATED);
-    CloseWindow();
-    //TODO: if error, return non 0 and terminate active
-    return 0;
-}
-
 void update_launcher_input()
 {
-    if (IsKeyPressed(BIND_QUIT))
-    {
-        state &= ~STATE_LAUNCHER;
+    if (IsKeyPressed(KEY_Q))
         state &= ~STATE_ACTIVE;
-    }
 }
--- src/launcher/launcher.h
+++ src/launcher/launcher.h
@@ -1,21 +1,36 @@
 #ifndef LAUNCHER_H
 
+#include <raylib.h>
+
 #define VECTOR2_TYPES
 #include "../h/defines.h"
 
-#define MC_C_LAUNCHER_VERSION "Launcher 0.1.0"
+#define WIDTH 1280
+#define HEIGHT 720
+#define MARGIN 20
 #define color(r, g, b, v, a) (Color){((f32)r/255)*v, ((f32)g/255)*v, ((f32)b/255)*v, a}
 
+// ---- states -----------------------------------------------------------------
+enum States
+{
+    STATE_ACTIVE =          0x01,
+}; /* States */
+
 // ---- colors -----------------------------------------------------------------
-#define COL_LAUNCHER_BG color(0x30, 0x30, 0x30, 0xFF, 0xFF)
+#define COL_BG              color(0x30, 0x30, 0x30, 0xFF, 0xFF)
+#define COL_BUTTON          color(0x26, 0x26, 0x26, 0xFF, 0xFF)
+#define COL_BUTTON_HOVER    color(0x2d, 0x2d, 0x2d, 0xFF, 0xFF)
 
 // ---- declarations -----------------------------------------------------------
+extern u8 state;
 extern v2i16 render_size;
+extern Vector2 cursor;
+#define detect_cursor cursor = GetMousePosition();
 
 // ---- signatures -------------------------------------------------------------
 void init_launcher();
 void update_launcher();
-u8 close_launcher();
+void close_launcher();
 void update_launcher_input();
 
 #define LAUNCHER_H
