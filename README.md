aedf4a4fe90eaaf080b227fc18ed6aeff696c281
lilya <lily.awertnex@gmail.com>
2025-04-02 11:38:15 +0300

    Fix build system arrays' garbage values

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # minecraft.c
-- - - -
-**a 1:1 minecraft clone but in C, as best as I could, with minimal library usage.
+**a 1:1 minecraft clone but in C, as best as I could, with minimal library usage, if any.
 eventually I will replace all minecraft resources with my own, for copyright reasons.**
 
 ## dependencies
 - [raylib](https://github.com/raysan5/raylib) by [raysan5](https://github.com/raysan5)
 
+## requirements
+- c compiler 'cc' or 'gcc'
+
 ## build and run
-#### linux:
+#### for both linux and windows:
 ```
 git clone https://github.com/awertnex/minecraft.c.git
-cd minecraft.c
-chmod 775 ./build.sh
-./build.sh
+cd minecraft.c/
+cc src/build.c -o build -std=c99
+./build
 ```
-and to run it:
-```
-./minecraft
-```
-additionally, following the command with a name (e.g. `./minecraft "minecraft.c 0.1.0"` creates a minecraft.c instance directory with that name next to the executable, and if the instance exists it will open it
+if build successful, run minecraft.c with `./minecraft_c`
+
+- - - -
+**additional commands**
+- build launcher with `./build launcher` and run with `./launcher`
+    - the launcher creates a directory 'minecraft.c/' in your 'HOME' directory on linux or in your 'appdata' folder on windows
+    - all minecraft.c instances will be stored in 'minecraft.c/instances'
+    - the default path to 'minecraft.c/' can be changed from the file 'dir.c' in the function 'init_paths()' before building
+- list available tests with `./build list`, build test [n] with `./build test [n]`
+- help `./build --help`
--- src/build.c
+++ src/build.c
@@ -3,99 +3,35 @@
 #include <string.h>
 #include <unistd.h>
 
-char** cmd = {0};
+#if defined __linux__
+    #define PLATFORM_LINUX
+    #define EXECUTE(cmd, args) execv(cmd, args)
+#elif defined _WIN32 || defined _WIN64 || defined __CYGWIN__s
+    #define PLATFORM_WINDOWS
+    #define EXECUTE(cmd, args)
+#endif // PLATFORM
+
+int exit_code = 0;
+char** cmd;
 
 char str_exec[24] = "/usr/bin/cc";
 char str_main[32] = "main.c";
-char str_children[16][24] = {"logger.c", "assets.c", "chunking.c", "dir.c", "gui.c", "keymaps.c", "logic.c", "super_debugger.c"};
-char str_cflags[4][24] = {"-Wall", "-Wextra", "-ggdb", "-Wpedantic"};
-char str_libs[2][24] = {"-lraylib", "-lm"};
-char str_out[3][32] = {"-o", "../minecraft"};
+char str_children[9][24] = {"logger.c", "assets.c", "chunking.c", "dir.c", "gui.c", "keymaps.c", "logic.c", "super_debugger.c", NULL};
+char str_cflags[5][24] = {"-Wall", "-Wextra", "-ggdb", "-Wpedantic", NULL};
+char str_libs[3][24] = {"-lraylib", "-lm", NULL};
+char str_out[3][32] = {"-o", "../minecraft_c", NULL};
 char str_tests[3][24] =
 {
     "chunk_loader",
     "function_pointer",
+    NULL,
 };
 
-void append_cmd();
-
-int main(int argc, char **argv) // ---- main -----------------------------------
+void build_cmd()
 {
-    cmd = (char**)calloc(64, sizeof(char*));
-    if (argc > 1)
-    {
-        if (!strncmp(argv[1], "list", 4))
-        {
-            printf("List: \n  %s\n",
-                    "launcher");
-            printf("Tests: \n");
-            for (int i = 0; i < 64 && str_tests[i][0]; ++i)
-                printf("  test %03d: %s\n", i, str_tests[i]);
-            free(cmd);
-            return 0;
-        }
-        else if (!strncmp(argv[1], "launcher", 8))
-        {
-            printf("Building minecraft.c launcher..\n");
-
-            snprintf(str_main, 32, "launcher/launcher.c");
-            memset(str_children, 0, sizeof(str_children));
-            memset(str_libs[1], 0, sizeof(str_libs[1]));
-            snprintf(str_out[1], 32, "../launcher");
-        }
-        else if (!strncmp(argv[1], "test", 4))
-        {
-            if (!argv[2])
-            {
-                printf("usage: ./build test [n]\n");
-                free(cmd);
-                return 0;
-            }
-
-            if (((argv[2][0] - 48) < 0) || ((argv[2][0] - 48) > sizeof(str_tests)/sizeof(str_tests[0])))
-            {
-                printf("Invalid '%c', Try './build list' to List Available Options..\n", argv[2][0]);
-                free(cmd);
-                return 0;
-            }
-
-            printf("Building test %03d: %s..\n", (argv[2][0]) - 48, str_tests[(argv[2][0]) - 48]);
-            snprintf(str_main, 32, "tests/%s.c", str_tests[(argv[2][0]) - 48]);
-            memset(str_children, 0, sizeof(str_children));
-            snprintf(str_out[1], 32, "../test_%s", str_tests[(argv[2][0]) - 48]);
-        }
-        else if (!strncmp(argv[1], "help", 4))
-        {
-            printf("usages:\n  %s\n  %s\n  %s\n  %s\n",
-                    "./build list       (list all available options and tests)",
-                    "./build test [n]   (build test 'n' from the 'list' command)",
-                    "./build launcher   (build the launcher)",
-                    "./build help       (print this list again)");
-            free(cmd);
-            return 0;
-        }
-        else
-        {
-            printf("Invalid '%s', Try './build help' to List Available Options..\n", argv[1]);
-            free(cmd);
-            return 0;
-        }
-    }
-    else printf("Building main.c..\n");
-
-    append_cmd();
-
-    if (!chdir("src/"))
-        execv("/usr/bin/cc", (char* const*)cmd);
-    else printf("Error: 'src/' Directory Not Found");
-
-    free(cmd);
-    return 0;
-}
-
-void append_cmd()
-{
-    for (int i = 2, j = 0, stage = 0; i < 64; ++i)
+    cmd = (char**) malloc(64*sizeof(char*));
+    memset(cmd, 0, 64*sizeof(char*));
+    for (int i = 2, j = 0, stage = 0, parse = 1; i < 64 && parse; ++i)
     {
         switch (stage)
         {
@@ -130,7 +66,7 @@ void append_cmd()
                 ++j;
                 break;
 
-            case 4:
+            case 3:
                 if (!str_libs[j][0])
                 {
                     ++stage;
@@ -142,15 +78,91 @@ void append_cmd()
                 ++j;
                 break;
 
-            case 3:
+            case 4:
                 if (!str_out[j][0])
                 {
                     cmd[i] = NULL;
-                    return;
+                    parse = 0;
+                    break;
                 }
                 cmd[i] = str_out[j];
                 ++j;
                 break;
         }
     }
+
+    if (!chdir("src/"))
+        EXECUTE("/usr/bin/cc", cmd);
+    else
+    {
+        printf("Error: 'src/' Directory Not Found");
+        exit_code = -1;
+    }
+
+    free(cmd);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        printf("Building minecraft.c..\n");
+        build_cmd();
+        if (exit_code) return exit_code;
+        return 0;
+    }
+
+    if (!strncmp(argv[1], "list", 4))
+    {
+        printf("List: \n  %s\n",
+                "launcher");
+        printf("Tests: \n");
+        for (int i = 0; i < 64 && str_tests[i][0]; ++i)
+            printf("  test %03d: %s\n", i, str_tests[i]);
+    }
+    else if (!strncmp(argv[1], "launcher", 8))
+    {
+        printf("Building minecraft.c launcher..\n");
+        snprintf(str_main, 32, "launcher/launcher.c");
+        memset(str_children, 0, sizeof(str_children));
+        memset(str_libs[1], 0, sizeof(str_libs[1]));
+        snprintf(str_out[1], 32, "../launcher");
+        build_cmd();
+        if (exit_code) return exit_code;
+    }
+    else if (!strncmp(argv[1], "test", 4))
+    {
+        if (!argv[2])
+        {
+            printf("usage: ./build test [n]\n");
+            return -1;
+        }
+        else if (((argv[2][0] - 48) < 0) || ((argv[2][0] - 48) > sizeof(str_tests)/sizeof(str_tests[0])))
+        {
+            printf("Invalid '%c', Try './build list' to List Available Options..\n", argv[2][0]);
+            return -1;
+        }
+
+        printf("Building test %03d '%s'..\n", (argv[2][0]) - 48, str_tests[argv[2][0] - 48]);
+        snprintf(str_main, 32, "tests/%s.c", str_tests[argv[2][0] - 48]);
+        memset(str_children, 0, sizeof(str_children));
+        snprintf(str_out[1], 32, "../test_%s", str_tests[argv[2][0] - 48]);
+        build_cmd();
+        if (exit_code) return exit_code;
+    }
+    else if (!strncmp(argv[1], "--help", 6))
+    {
+        printf("usages:\n  %s\n  %s\n  %s\n  %s\n",
+                "./build launcher   (build the launcher)",
+                "./build list       (list all available options and tests)",
+                "./build test [n]   (build test 'n' from the 'list' command)",
+                "./build help       (print this list again)");
+    }
+    else
+    {
+        printf("Invalid '%s', Try './build help' to List Available Options..\n", argv[1]);
+        return -1;
+    }
+
+    return 0;
 }
