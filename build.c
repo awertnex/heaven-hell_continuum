615954d4ddcba5c0be8cd23be93da433cc1a7730
lilya <lily.awertnex@gmail.com>
2025-04-02 11:50:09 +0300

    Add windows support

---
+++ build.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#if defined __linux__
+    #define PLATFORM_LINUX
+    #define EXECUTEVP(cmd, args) execvp(cmd, args)
+#elif defined _WIN32 || defined _WIN64 || defined __CYGWIN__s
+    #define PLATFORM_WINDOWS
+    #define EXECUTEVP(cmd, args) _execvp(cmd, args)
+#endif // PLATFORM
+
+int exit_code = 0;
+char** cmd;
+
+char str_exec[24] = "cc";
+char str_main[32] = "main.c";
+char str_children[9][24] = {"logger.c", "assets.c", "chunking.c", "dir.c", "gui.c", "keymaps.c", "logic.c", "super_debugger.c", NULL};
+char str_cflags[5][24] = {"-Wall", "-Wextra", "-ggdb", "-Wpedantic", NULL};
+char str_libs[3][24] = {"-lraylib", "-lm", NULL};
+char str_out[3][32] = {"-o", "../minecraft_c", NULL};
+char str_tests[3][24] =
+{
+    "chunk_loader",
+    "function_pointer",
+    NULL,
+};
+
+void build_cmd()
+{
+    cmd = (char**) malloc(64*sizeof(char*));
+    memset(cmd, 0, 64*sizeof(char*));
+    for (int i = 2, j = 0, stage = 0, parse = 1; i < 64 && parse; ++i)
+    {
+        switch (stage)
+        {
+            case 0:
+                cmd[0] = str_exec;
+                cmd[1] = str_main;
+                ++stage;
+                --i;
+                break;
+
+            case 1:
+                if (!str_children[j][0])
+                {
+                    ++stage;
+                    --i;
+                    j = 0;
+                    continue;
+                }
+                cmd[i] = str_children[j];
+                ++j;
+                break;
+
+            case 2:
+                if (!str_cflags[j][0])
+                {
+                    ++stage;
+                    --i;
+                    j = 0;
+                    continue;
+                }
+                cmd[i] = str_cflags[j];
+                ++j;
+                break;
+
+            case 3:
+                if (!str_libs[j][0])
+                {
+                    ++stage;
+                    --i;
+                    j = 0;
+                    continue;
+                }
+                cmd[i] = str_libs[j];
+                ++j;
+                break;
+
+            case 4:
+                if (!str_out[j][0])
+                {
+                    cmd[i] = NULL;
+                    parse = 0;
+                    break;
+                }
+                cmd[i] = str_out[j];
+                ++j;
+                break;
+        }
+    }
+
+    if (!chdir("src/"))
+        EXECUTEVP("cc", cmd);
+    else
+    {
+        printf("Error: 'src/' Directory Not Found");
+        exit_code = -1;
+    }
+
+    free(cmd);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        printf("Building minecraft.c..\n");
+        build_cmd();
+        if (exit_code) return exit_code;
+        return 0;
+    }
+
+    if (!strncmp(argv[1], "list", 4))
+    {
+        printf("List: \n  %s\n",
+                "launcher");
+        printf("Tests: \n");
+        for (int i = 0; i < 64 && str_tests[i][0]; ++i)
+            printf("  test %03d: %s\n", i, str_tests[i]);
+    }
+    else if (!strncmp(argv[1], "launcher", 8))
+    {
+        printf("Building minecraft.c launcher..\n");
+        snprintf(str_main, 32, "launcher/launcher.c");
+        memset(str_children, 0, sizeof(str_children));
+        memset(str_libs[1], 0, sizeof(str_libs[1]));
+        snprintf(str_out[1], 32, "../launcher");
+        build_cmd();
+        if (exit_code) return exit_code;
+    }
+    else if (!strncmp(argv[1], "test", 4))
+    {
+        if (!argv[2])
+        {
+            printf("usage: ./build test [n]\n");
+            return -1;
+        }
+        else if (((argv[2][0] - 48) < 0) || ((argv[2][0] - 48) > sizeof(str_tests)/sizeof(str_tests[0])))
+        {
+            printf("Invalid '%c', Try './build list' to List Available Options..\n", argv[2][0]);
+            return -1;
+        }
+
+        printf("Building test %03d '%s'..\n", (argv[2][0]) - 48, str_tests[argv[2][0] - 48]);
+        snprintf(str_main, 32, "tests/%s.c", str_tests[argv[2][0] - 48]);
+        memset(str_children, 0, sizeof(str_children));
+        snprintf(str_out[1], 32, "../test_%s", str_tests[argv[2][0] - 48]);
+        build_cmd();
+        if (exit_code) return exit_code;
+    }
+    else if (!strncmp(argv[1], "--help", 6))
+    {
+        printf("usages:\n  %s\n  %s\n  %s\n  %s\n",
+                "./build launcher   (build the launcher)",
+                "./build list       (list all available options and tests)",
+                "./build test [n]   (build test 'n' from the 'list' command)",
+                "./build help       (print this list again)");
+    }
+    else
+    {
+        printf("Invalid '%s', Try './build help' to List Available Options..\n", argv[1]);
+        return -1;
+    }
+
+    return 0;
+}
