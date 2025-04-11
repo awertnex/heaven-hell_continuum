95cf4101196d2e962736629ff1054c4a385911af
lilya <lily.awertnex@gmail.com>
2025-04-11 08:00:23 +0300

    Tidy up defines

--- src/h/defines.h
+++ src/h/defines.h
@@ -27,171 +27,141 @@ typedef bool        b32;
 
 #ifdef VECTOR2_TYPES
 // ---- vector2u ---------------------------------------------------------------
-typedef struct v2u8
-{
+typedef struct {
 	u8 x, y;
 } v2u8;
 
-typedef struct v2u16
-{
+typedef struct {
 	u16 x, y;
 } v2u16;
 
-typedef struct v2u32
-{
+typedef struct {
 	u32 x, y;
 } v2u32;
 
-typedef struct v2u64
-{
+typedef struct {
 	u64 x, y;
 } v2u64;
 
 // ---- vector2i ---------------------------------------------------------------
-typedef struct v2i8
-{
+typedef struct {
 	i8 x, y;
 } v2i8;
 
-typedef struct v2i16
-{
+typedef struct {
 	i16 x, y;
 } v2i16;
 
-typedef struct v2i32
-{
+typedef struct {
 	i32 x, y;
 } v2i32;
 
-typedef struct v2i64
-{
+typedef struct {
 	i64 x, y;
 } v2i64;
 
 // ---- vector2f ---------------------------------------------------------------
-typedef struct v2f32
-{
+typedef struct {
 	f32 x, y;
 } v2f32;
 
-typedef struct v2f64
-{
+typedef struct {
 	f64 x, y;
 } v2f64;
 
-#endif /* VECTOR2_TYPES */
+#endif // VECTOR2_TYPES
 
 #ifdef VECTOR3_TYPES
 // ---- vector3u ---------------------------------------------------------------
-typedef struct v3u8
-{
+typedef struct {
 	u8 x, y, z;
 } v3u8;
 
-typedef struct v3u16
-{
+typedef struct {
 	u16 x, y, z;
 } v3u16;
 
-typedef struct v3u32
-{
+typedef struct {
 	u32 x, y, z;
 } v3u32;
 
-typedef struct v3u64
-{
+typedef struct {
 	u64 x, y, z;
 } v3u64;
 
 // ---- vector3i ---------------------------------------------------------------
-typedef struct v3i8
-{
+typedef struct {
 	i8 x, y, z;
 } v3i8;
 
-typedef struct v3i16
-{
+typedef struct {
 	i16 x, y, z;
 } v3i16;
 
-typedef struct v3i32
-{
+typedef struct {
 	i32 x, y, z;
 } v3i32;
 
-typedef struct v3i64
-{
+typedef struct {
 	i64 x, y, z;
 } v3i64;
 
 // ---- vector3f ---------------------------------------------------------------
-typedef struct v3f32
-{
+typedef struct {
 	f32 x, y, z;
 } v3f32;
 
-typedef struct v3f64
-{
+typedef struct {
 	f64 x, y, z;
 } v3f64;
 
-#endif /* VECTOR3_TYPES */
+#endif // VECTOR3_TYPES
 
 #ifdef VECTOR4_TYPES
 // ---- vector4u ---------------------------------------------------------------
-typedef struct v4u8
-{
+typedef struct {
 	u8 x, y, z, w;
 } v4u8;
 
-typedef struct v4u16
-{
+typedef struct {
 	u16 x, y, z, w;
 } v4u16;
 
-typedef struct v4u32
-{
+typedef struct {
 	u32 x, y, z, w;
 } v4u32;
 
-typedef struct v4u64
-{
+typedef struct {
 	u64 x, y, z, w;
 } v4u64;
 
 // ---- vector4i ---------------------------------------------------------------
-typedef struct v4i8
-{
+typedef struct {
 	i8 x, y, z, w;
 } v4i8;
 
-typedef struct v4i16
-{
+typedef struct {
 	i16 x, y, z, w;
 } v4i16;
 
-typedef struct v4i32
-{
+typedef struct {
 	i32 x, y, z, w;
 } v4i32;
 
-typedef struct v4i64
-{
+typedef struct {
 	i64 x, y, z, w;
 } v4i64;
 
 // ---- vector3f ---------------------------------------------------------------
-typedef struct v4f32
-{
+typedef struct {
 	f32 x, y, z, w;
 } v4f32;
 
-typedef struct v4f64
-{
+typedef struct {
 	f64 x, y, z, w;
 } v4f64;
 
-#endif /* VECTOR4_TYPES */
+#endif // VECTOR4_TYPES
 
 #define DEFINES_H
 #endif
