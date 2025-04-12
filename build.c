f62a542c4d72475b4981060fcc3fa3c0877d52f0
lilya <lily.awertnex@gmail.com>
2025-04-12 22:40:42 +0300

    Add new test chunk_pointer_table

--- build.c
+++ build.c
@@ -29,9 +29,10 @@ int exit_code = 0;
 char str_main[32] = "main.c";
 char str_cflags[8][24] = {"-Wall", "-Wextra", "-ggdb", "-Wno-missing-braces", "-Wpedantic", "-std=c99", "-fno-builtin", 0};
 char str_out[3][32] = {"-o", EXECUTABLE, 0};
-char str_tests[3][24] =
+char str_tests[4][24] =
 {
     "chunk_loader",
+    "chunk_pointer_table",
     "function_pointer",
     0,
 };
