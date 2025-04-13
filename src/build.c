be3700cef834886724cd85409174eac4211fde02
lilya <lily.awertnex@gmail.com>
2025-04-13 03:23:49 +0300

    Organized build tool

--- src/build.c
+++ src/build.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <dirent.h>
+
+#define DIR_BIN "bin"
 
 #if defined __linux__
     #define ALLOC_CMD (cmd = (char**) malloc(64*sizeof(char*)))
     #define ZERO_CMD (memset(cmd, 0, 64*sizeof(char*)))
     #define FREE_CMD free(cmd)
     #define COMPILER "cc"
-    #define EXECUTABLE "../minecraft_c"
     #define EXTENSION ""
+    #define MKDIR_BIN mkdir("bin/", 0775);
 char** cmd;
 char str_libs[3][24] = {"-lraylib", "-lm", 0};
 
@@ -18,17 +22,15 @@ char str_libs[3][24] = {"-lraylib", "-lm", 0};
     #define ZERO_CMD
     #define FREE_CMD
     #define COMPILER "gcc"
-    #define EXECUTABLE "../minecraft_c.exe"
     #define EXTENSION ".exe"
+    #define MKDIR_BIN mkdir("bin");
 char* cmd[64];
 char str_libs[5][24] = {"-lraylib", "-lgdi32", "-lwinmm", "-lm", 0};
 #endif // PLATFORM
 
-int exit_code = 0;
-
 char str_main[32] = "main.c";
 char str_cflags[8][24] = {"-Wall", "-Wextra", "-ggdb", "-Wno-missing-braces", "-Wpedantic", "-std=c99", "-fno-builtin", 0};
-char str_out[3][32] = {"-o", EXECUTABLE, 0};
+char str_out[3][32] = {"-o", "../bin/minecraft_c", 0};
 char str_tests[4][24] =
 {
     "chunk_loader",
@@ -37,7 +39,16 @@ char str_tests[4][24] =
     0,
 };
 
-void build_cmd()
+int is_dir_exists(const char *path)
+{
+    struct stat stats;
+    if (stat(path, &stats) == 0)
+        if (S_ISDIR(stats.st_mode))
+            return 1;
+    return 0;
+}
+
+int build_cmd()
 {
     ALLOC_CMD;
     ZERO_CMD;
@@ -93,22 +104,28 @@ void build_cmd()
         execvp(COMPILER, cmd);
     else
     {
-        printf("Error: 'src/' Directory Not Found\n");
-        exit_code = -1;
+        printf("Error: 'src/' Directory Not Found, Aborting Build\n");
+        return 1;
     }
 
     FREE_CMD;
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
     perror("execvp");
+    if (!is_dir_exists(DIR_BIN))
+    {
+        MKDIR_BIN;
+        printf("Directory 'bin/' Created!\n");
+    }
+
 
     if (argc == 1)
     {
         printf("Building minecraft.c..\n");
-        build_cmd();
-        if (exit_code) return exit_code;
+        if (build_cmd()) return -1;
         return 0;
     }
 
@@ -125,9 +142,8 @@ int main(int argc, char **argv)
         printf("Building minecraft.c launcher..\n");
         snprintf(str_main, 32, "launcher/launcher.c");
         memset(str_libs[1], 0, sizeof(str_libs[1]));
-        snprintf(str_out[1], 32, "../launcher%s", EXTENSION);
-        build_cmd();
-        if (exit_code) return exit_code;
+        snprintf(str_out[1], 32, "../bin/launcher%s", EXTENSION);
+        if(build_cmd()) return -1;
     }
     else if (!strncmp(argv[1], "test", 4))
     {
@@ -138,15 +154,14 @@ int main(int argc, char **argv)
         }
         else if ((atoi(argv[2]) < 0) || (atoi(argv[2]) >= (sizeof(str_tests)/sizeof(str_tests[0])) - 1))
         {
-            printf("Invalid '%c', Try './build list' to List Available Options..\n", argv[2][0]);
+            printf("'%c' Invalid, Try './build list' to List Available Options..\n", argv[2][0]);
             return -1;
         }
 
         printf("Building test %03d '%s'..\n", (argv[2][0]) - 48, str_tests[argv[2][0] - 48]);
         snprintf(str_main, 32, "tests/%s.c", str_tests[argv[2][0] - 48]);
         snprintf(str_out[1], 32, "../test_%s%s", str_tests[argv[2][0] - 48], EXTENSION);
-        build_cmd();
-        if (exit_code) return exit_code;
+        if (build_cmd()) return -1;
     }
     else if (!strncmp(argv[1], "--help", 6))
     {
