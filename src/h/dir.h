15025f1a3671ea10fdc3b4d73cbc2ebba0b9e32a
lilya <lily.awertnex@gmail.com>
2025-05-19 05:31:26 +0300

    Fix syntax and make simpler

--- src/h/dir.h
+++ src/h/dir.h
@@ -12,39 +12,55 @@
 #endif // PATH_MAX
 // ---- from linux/limits.h ----------------------------------------------------
 
-enum DirectoryStructure
+enum Directories
 {
-    MC_C_DIR_BIN = 0,
-    MC_C_DIR_SAVES = 1,
-    MC_C_DIR_RESOURCES = 2,
-    MC_C_DIR_TEXTURES = 3,
-    MC_C_DIR_BLOCK = 4,
-    MC_C_DIR_ENVIRONMENT = 5,
-    MC_C_DIR_ENTITY = 6,
-    MC_C_DIR_FONT = 7,
-    MC_C_DIR_GUI = 8,
-    MC_C_DIR_ITEM = 9,
-    MC_C_DIR_MISC = 10,
-    MC_C_DIR_PAINTING = 11,
-    MC_C_DIR_SOUNDS = 12,
-    MC_C_DIR_INFO = 13,
-    MC_C_DIR_SCREENSHOTS = 14,
+    /* Grandpath Directories */
+    DIR_INSTANCES =         0,
 
-    MC_C_DIR_ADVANCEMENTS = 0,
-    MC_C_DIR_CHUNKS = 1,
-    MC_C_DIR_ENTITIES = 2,
-}; /* DirectoryStructure */
+    /* Instance Directories */
+    DIR_BIN =               0,
+    DIR_MODELS =            1,
+    DIR_RESOURCES =         2,
+    DIR_TEXTURES =          3,
+    DIR_BLOCKS =            4,
+    DIR_ENVIRONMENT =       5,
+    DIR_ENTITIES =          6,
+    DIR_FONT =              7,
+    DIR_GUI =               8,
+    DIR_ITEMS =             9,
+    DIR_LOGO =              10,
+    DIR_MISC =              11,
+    DIR_PAINTINGS =         12,
+    DIR_SOUNDS =            13,
+    DIR_INFO =              14,
+    DIR_SAVES =             15,
+    DIR_SCREENSHOTS =       16,
+    DIR_TEXT =              17,
+
+    /* World Directories */
+    DIR_ADVANCEMENTS =      0,
+    DIR_CHUNKS =            1,
+    DIR_ENTITIES_WORLD =    2,
+    DIR_LOGS =              3,
+    DIR_PLAYER_DATA =       4,
+}; /* Directories */
+
+#define GRANDPATH_DIR_COUNT 1
+#define INSTANCE_DIR_COUNT  18
+#define WORLD_DIR_COUNT     5
 
 // ---- declarations -----------------------------------------------------------
 extern str mc_c_grandpath[PATH_MAX];
 extern str mc_c_subpath[PATH_MAX];
 extern str mc_c_launcher_path[PATH_MAX];
-extern str instance_dir_structure[17][NAME_MAX];
-extern str world_dir_structure[3][NAME_MAX];
+extern str grandpath_dir[GRANDPATH_DIR_COUNT][NAME_MAX];
+extern str instance_dir[INSTANCE_DIR_COUNT][NAME_MAX];
+extern str world_dir[WORLD_DIR_COUNT][NAME_MAX];
 
 // ---- signatures -------------------------------------------------------------
 void init_paths();
-void init_instance_directory(str *instance_name, u16 *state, u8 STATE_ACTIVE);
+int is_dir_exists(const char *path);
+int init_instance_directory(str *instance_name);
 void init_world_directory(str *world_name);
 
 #define MC_C_DIR_H
