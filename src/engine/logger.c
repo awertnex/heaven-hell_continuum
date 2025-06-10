e10dc89c88c11699895714b95892a7908e7528ea
lilya <lily.awertnex@gmail.com>
2025-06-10 15:31:53 +0300

    Colored logger output

--- src/engine/logger.c
+++ src/engine/logger.c
@@ -3,7 +3,7 @@
 
 #include "h/logger.h"
 
-static str log_tag[6][13] =
+static str log_tag[][16] =
 {
     "FATAL",
     "ERROR",
@@ -11,6 +11,15 @@ static str log_tag[6][13] =
     "INFO",
     "DEBUG",
     "TRACE",
+};
+str escape_open[][16] =
+{
+    "\033[1;0;31m",
+    "\033[1;0;91m",
+    "\033[1;0;95m",
+    "\033[0m",
+    "\033[0m",
+    "\033[0m",
 };
 
 b8 init_logger()
@@ -30,6 +39,7 @@ void log_output(u8 log_level, const str* format, ...)
     //b8 is_error = log_level < 2;
     str in_message[4096] = {0};
     str out_message[8192] = {0};
+    str escape_close[8] = "\033[0m";
 
     __builtin_va_list args;
     va_start(args, format);
@@ -37,6 +47,6 @@ void log_output(u8 log_level, const str* format, ...)
     va_end(args);
 
     snprintf(out_message, 6144, "%s: %s", log_tag[log_level], in_message);
-    fprintf(stderr, "%s", out_message);
+    fprintf(stderr, "%s%s%s", escape_open[log_level], out_message, escape_close);
 }
 
