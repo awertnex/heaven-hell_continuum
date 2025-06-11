ef3908126c9a27ab600b01dbf5f499f2ee8acfb4
lilya <lily.awertnex@gmail.com>
2025-06-12 02:37:10 +0300

    Fix get_dir_contents(), Add is_dir()

--- src/engine/dir.c
+++ src/engine/dir.c
@@ -27,6 +27,14 @@ b8 is_file_exists(const str *file_name)
     return FALSE;
 }
 
+b8 is_dir(const str *name)
+{
+    struct stat stats;
+    if (stat(name, &stats) == 0 && S_ISDIR(stats.st_mode))
+        return TRUE;
+    return FALSE;
+}
+
 b8 is_dir_exists(const str *dir_name)
 {
     struct stat stats;
@@ -46,10 +54,13 @@ b8 is_dir_exists(const str *dir_name)
 
 str *get_file_contents(const str *file_name)
 {
-    FILE *file;
+    if (!is_file_exists(file_name))
+            return NULL;
+
+    FILE *file = NULL;
     if ((file = fopen(file_name, "r")) == NULL)
     {
-        LOGERROR("File '%s' Not Found\n", file_name);
+        LOGERROR("File '%s' Not Accessible\n", file_name);
         return NULL;
     }
 
@@ -57,13 +68,13 @@ str *get_file_contents(const str *file_name)
     u64 len = ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    str *contents = NULL;
-    if (!mem_alloc((void*)&contents, len + 1, "file_contents"))
+    str *file_contents = NULL;
+    if (!mem_alloc((void*)&file_contents, len + 1, "file_contents"))
         goto cleanup;
-    fread(contents, 1, len, file);
-    contents[len] = '\0';
+    fread(file_contents, 1, len, file);
+    file_contents[len] = 0;
     fclose(file);
-    return contents;
+    return file_contents;
 
 cleanup:
     fclose(file);
@@ -71,51 +82,57 @@ cleanup:
 }
 
 /*
+ * returns directory contents inside dir_name;
  * count = return number of entries inside directory;
  */
-str **get_dir_contents(const str *dir_name, u64 *count)
+str_buf get_dir_contents(const str *dir_name)
 {
     if (!dir_name || !is_dir_exists(dir_name))
-        return NULL;
+        return (str_buf){NULL};
 
-    str *path_absolute = get_path_absolute(dir_name);
-    if (!path_absolute)
+    str *dir_name_absolute = get_path_absolute(dir_name);
+    if (!dir_name_absolute)
         goto cleanup;
 
+    str dir_name_absolute_usable[PATH_MAX] = {0};
+    snprintf(dir_name_absolute_usable, PATH_MAX, "%s", dir_name_absolute);
+
     DIR *dir = NULL;
-    u64 file_count = 0;
     struct dirent *entry;
-    str **contents = NULL;
+    str_buf contents = {NULL};
 
-    dir = opendir(path_absolute);
+    dir = opendir(dir_name_absolute);
     while ((entry = readdir(dir)))
-        ++file_count;
+        ++contents.count;
 
-    if (!file_count || !mem_alloc_memb((void*)&contents, file_count, NAME_MAX, "dir_contents"))
+    if (!contents.count
+            || !mem_alloc((void*)&contents.entry, contents.count * sizeof(str*), "dir_contents")
+            || !mem_alloc_memb((void*)&contents.buf, contents.count, NAME_MAX, "dir_contents_buffer"))
         goto cleanup;
 
     rewinddir(dir);
     u64 i = 0;
+    str path_full[PATH_MAX] = {0};
     while ((entry = readdir(dir)))
     {
-        contents[i] = entry->d_name;
+        contents.entry[i] = contents.buf + (i * NAME_MAX);
+        memcpy(contents.entry[i], entry->d_name, NAME_MAX - 1);
+        snprintf(path_full, PATH_MAX, "%s%s", dir_name_absolute_usable, entry->d_name);
+        if (is_dir(path_full))
+            check_slash(contents.entry[i]);
         ++i;
     }
 
-    if (count != NULL)
-        *count = file_count;
-
     closedir(dir);
-    free(path_absolute);
+    mem_free((void*)&dir_name_absolute, strlen(dir_name_absolute), "dir_name_absolute");
     return contents;
 
 cleanup:
-    if (path_absolute != NULL)
-        free(path_absolute);
-
     if (dir != NULL)
         closedir(dir);
-    return NULL;
+    mem_free((void*)&dir_name_absolute, strlen(dir_name_absolute), "dir_name_absolute");
+    mem_free((void*)&contents, sizeof(contents), "dir_contents");
+    return (str_buf){NULL};
 }
 
 str *get_path_absolute(const str *path)
@@ -129,22 +146,24 @@ str *get_path_absolute(const str *path)
     if (!is_dir_exists(path))
         return NULL;
 
-    str path_resolved[PATH_MAX] = {0};
-    if (!realpath(path, path_resolved))
+    str path_absolute[PATH_MAX] = {0};
+    if (!realpath(path, path_absolute))
         return NULL;
 
-    u64 len = strlen(path_resolved);
-    str *path_absolute = NULL;
-    if (!mem_alloc((void*)&path_absolute, len + 2, "path_absolute"))
+    u64 len = strlen(path_absolute);
+    str *result = NULL;
+    if (!mem_alloc((void*)&result, len + 1, "path_absolute"))
         return NULL;
 
-    strncpy(path_absolute, path_resolved, len);
-    if (path_absolute[len - 1] != '/')
-        strcat(path_absolute, "/\0");
+    strncpy(result, path_absolute, len);
+    check_slash(result);
 
-    return path_absolute;
+    return result;
 }
 
+/*
+ * returns calloc'd string of the executable's current directory, slash and null terminated;
+ */
 str *get_path_bin_root(void)
 {
     str path_bin_root[PATH_MAX] = {0};
@@ -155,13 +174,43 @@ str *get_path_bin_root(void)
     }
 
     u64 len = strlen(path_bin_root);
+    if (len >= PATH_MAX - 1)
+    {
+        LOGFATAL("Path Too Long '%s', Process Aborted", path_bin_root);
+        return NULL;
+    }
+
     path_bin_root[len] = 0;
     str *result = NULL;
-    if (!mem_alloc((void*)&result, len, "path_bin_root"))
+    if (!mem_alloc((void*)&result, len + 1, "path_bin_root"))
         return NULL;
 
-    strncpy(result, path_bin_root, PATH_MAX);
+    strncpy(result, path_bin_root, len);
+
+    char *last_slash = strrchr(result, '/');
+    if (last_slash)
+        *last_slash = 0;
+    check_slash(result);
 
     return result;
+}
+
+/*
+ * appends '/' onto path if there's none, null terminated;
+ */
+void check_slash(str *path)
+{
+    if (path == NULL)
+        return;
+
+    u64 len = strlen(path);
+    if (len >= PATH_MAX)
+        return;
+
+    if (path[len - 1] == '/')
+        return;
+
+    path[len] = '/';
+    path[len + 1] = 0;
 }
 
--- src/engine/h/dir.h
+++ src/engine/h/dir.h
@@ -4,16 +4,20 @@
 #include <sys/stat.h>
 
 #include "defines.h"
+#include "memory.h"
 
 #define DIR_MAX     64
 
 /* ---- section: signatures ------------------------------------------------- */
+
 b8 is_file_exists(const str *file_name);
+b8 is_dir(const str *name);
 b8 is_dir_exists(const str *dir_name);
 str *get_file_contents(const str *file_name);
-str **get_dir_contents(const str *dir_name, u64 *count);
+str_buf get_dir_contents(const str *dir_name);
 str *get_path_absolute(const str *path);
 str *get_path_bin_root(void);
 int make_dir(str *path, u16 mode);
+void check_slash(str *path);
 
 #endif /* MC_C_ENGINE_DIR_H */
