117e92696a686b58b70f33fa6eeaae2982ae3e1d
lilya <lily.awertnex@gmail.com>
2025-06-25 16:17:26 -0400

    Add limits.h platform-abstraction header

---
+++ src/engine/h/limits.h
@@ -0,0 +1,12 @@
+#ifndef ENGINE_LIMITS_H
+#define ENGINE_LIMITS_H
+
+#if defined(__linux__) || defined(__linux)
+    #include <linux/limits.h>
+#elif defined(__WIN32) || defined(__WIN64) || defined (__CYGWIN__)
+    #include <limits.h>
+    #define NAME_MAX 255
+#endif /* PLATFORM */
+
+#endif /* ENGINE_LIMITS_H */
+
