5b98ed940d53b5c30b945de3ada1d1f952645631
lily <lily.awertnex@gmail.com>
2025-10-22 16:26:21 +0300

    Build Tool: Merge build tool with engine, Separate game building and test building

--- build.c
+++ build.c
@@ -1,582 +1,111 @@
-#include <unistd.h>
-
-#include "engine/h/platform.h"
-#include "engine/h/defines.h"
-#include "engine/h/diagnostics.h"
-#include "engine/memory.c"
-#include "engine/dir.c"
-#include "engine/logger.c"
+#include "engine/build.c"
 
 #define DIR_ROOT        "Heaven-Hell Continuum/"
-#define DIR_ROOT_TESTS  "tests/"
 #define DIR_SRC         "src/"
-#define DIR_TESTS       DIR_SRC"tests/"
-#define CMD_MEMB        64
-#define CMD_SIZE        512
 #define ASSET_COUNT     3
+#define NEW_DIR_COUNT   2
 
-#if defined(__STDC_VERSION__)
-    #define STD __STDC_VERSION__
-#elif defined(__STDC__)
-    #define STD 89
-#endif /* STD */
-
-#define STD_C99 199901L
+u32 *const BUILD_ERR = &engine_err;
+str str_out_dir[CMD_SIZE] = {0}; /* bundle directory name */
 
 #if PLATFORM_LINUX
-#include "engine/platform_linux.c"
-
-#define COMPILER        "gcc"EXE
-#define STR_OUT         DIR_ROOT"hhc"
-
-str str_libs[][CMD_SIZE] =
-{
-    "-lm",
-    "-lglfw",
-    "-lfossil",
-};
-
+    #define STR_OUT     DIR_ROOT"hhc"
+    str str_libs[][CMD_SIZE] =
+    {
+        "-lm",
+        "-lglfw",
+        "-lfossil",
+    };
 #elif PLATFORM_WIN
-#include "engine/platform_windows.c"
-
-#define COMPILER        "gcc"EXE
-#define STR_OUT         "\""DIR_ROOT"hhc"EXE"\""
-
-str str_libs[][CMD_SIZE] =
-{
-    "-lgdi32",
-    "-lwinmm",
-    "-lm",
-    "-lglfw3",
-    "-lfossil",
-};
-
+    #define STR_OUT     "\""DIR_ROOT"hhc"EXE"\""
+    str str_libs[][CMD_SIZE] =
+    {
+        "-lgdi32",
+        "-lwinmm",
+        "-lm",
+        "-lglfw3",
+        "-lfossil",
+    };
 #endif /* PLATFORM */
-
-enum Flags
-{
-    STATE_TEST = 1,
-    STATE_ENGINE,
-
-    FLAG_SHOW_CMD = 1,
-    FLAG_RAW_CMD,
-}; /* Flags */
-
-/* ---- section: declarations ----------------------------------------------- */
-
-u32 engine_err = ERR_SUCCESS;
-u8 state = 0;
-u16 flags = 0;
-str *str_bin_root = NULL;
-str str_src[CMD_SIZE] = {0};            /* path: ./build.c */
-str str_bin[CMD_SIZE] = {0};            /* path: ./build%s, EXE */
-str str_bin_new[CMD_SIZE] = {0};        /* path: ./build_new%s, EXE */
-str str_bin_old[CMD_SIZE] = {0};        /* path: ./build_old%s, EXE */
-str str_dir_root[CMD_SIZE] = {0};       /* bundle directory name */
-buf cmd = {NULL};
-u64 cmd_pos = 0;
-buf str_tests = {NULL};
-
-str str_main[CMD_SIZE] = DIR_SRC"main.c";
-
-str str_cflags[][48] =
-{
-    ("-Wl,-rpath=$ORIGIN/lib/"PLATFORM),
-    "-std=c99",
-    "-Wall",
-    "-Wextra",
-    "-fno-builtin",
-};
-
-str str_out[CMD_SIZE] = STR_OUT;
-
-/* ---- section: signatures ------------------------------------------------- */
-
-void init_build(void);
-u32 is_source_changed(void);
-void rebuild_self(char **argv);
-u64 compare_argv(str *arg, int argc, str **argv);
-b8 evaluate_extension(const str *file_name);
-void strip_extension(const str *file_name, str *dest);
-void show_cmd(void);
-void raw_cmd(void);
-void push_cmd(const str *string);
-void build_game(void);
-void build_engine(void);
-void build_test(char **argv);
-void free_cmd(void);
-void fail_cmd(void);
-void help(void);
-void list(void);
-
-/* ---- section: functions -------------------------------------------------- */
-
-void
-init_build(void)
-{
-    str_bin_root = get_path_bin_root();
-    if (engine_err != ERR_SUCCESS)
-        fail_cmd();
-
-    check_slash(str_bin_root);
-    normalize_slash(str_bin_root);
-    snprintf(str_src, CMD_SIZE, "%sbuild.c", str_bin_root);
-    snprintf(str_bin, CMD_SIZE, "%sbuild%s", str_bin_root, EXE);
-    snprintf(str_bin_new, CMD_SIZE, "%sbuild_new%s", str_bin_root, EXE);
-    snprintf(str_bin_old, CMD_SIZE, "%sbuild_old%s", str_bin_root, EXE);
-}
-
-u32
-is_source_changed(void)
-{
-    unsigned long mtime_src = 0;
-    unsigned long mtime_bin = 0;
-
-    struct stat stats;
-
-    if (stat(str_src, &stats) == 0)
-        mtime_src = stats.st_mtime;
-    else
-    {
-        LOGERROR(ERR_FILE_NOT_FOUND, "%s\n", "File 'build.c' Not Found");
-        return engine_err;
-    }
-
-    if (stat(str_bin, &stats) == 0)
-        mtime_bin = stats.st_mtime;
-    else
-    {
-        LOGERROR(ERR_FILE_NOT_FOUND, "File 'build%s' Not Found\n", EXE);
-        return engine_err;
-    }
-
-    if (mtime_src && mtime_bin && mtime_src > mtime_bin)
-        return ERR_SUCCESS;
-
-    engine_err = ERR_SOURCE_NOT_CHANGE;
-    return engine_err;
-}
-
-void
-rebuild_self(char **argv)
-{
-    if (mem_alloc_buf(&cmd, 16, CMD_SIZE, "cmd") != ERR_SUCCESS)
-        fail_cmd();
-
-    LOGINFO("%s\n", "Rebuilding Self..");
-
-    snprintf(cmd.i[0], CMD_SIZE, "%s", COMPILER);
-    snprintf(cmd.i[1], CMD_SIZE, "%s", str_src);
-    normalize_slash(cmd.i[1]);
-    snprintf(cmd.i[2], CMD_SIZE, "%s", "-std=c99");
-    snprintf(cmd.i[3], CMD_SIZE, "%s", "-Wall");
-    snprintf(cmd.i[4], CMD_SIZE, "%s", "-Wextra");
-    snprintf(cmd.i[5], CMD_SIZE, "%s", "-fno-builtin");
-    snprintf(cmd.i[6], CMD_SIZE, "%s",
-            "-Wno-implicit-function-declaration");
-    snprintf(cmd.i[7], CMD_SIZE, "-I%s", str_bin_root);
-
-    snprintf(cmd.i[8], CMD_SIZE, "%s", "-o");
-    snprintf(cmd.i[9], CMD_SIZE, "%s", str_bin_new);
-    normalize_slash(cmd.i[9]);
-
-#if PLATFORM_LINUX
-    cmd.i[10] = NULL;
-#endif
-
-    if (exec(&cmd, "cmd") == ERR_SUCCESS)
-    {
-        LOGINFO("%s\n", "Self Rebuild Success");
-        rename(str_bin, str_bin_old);
-        rename(str_bin_new, str_bin);
-        remove(str_bin_old);
-
-        execvp(argv[0], (str *const *)argv);
-        LOGFATAL(engine_err, "'build%s' Failed, Process Aborted\n", EXE);
-        fail_cmd();
-    }
-
-    LOGFATAL(engine_err, "%s\n", "Self-Rebuild Failed, Process Aborted");
-    fail_cmd();
-}
-
-u64
-compare_argv(str *arg, int argc, char **argv)
-{
-    if (argc == 1)
-        return 0;
-
-    u32 i = 1;
-    for (; (int)i < argc; ++i)
-        if (!strncmp(argv[i], arg, strlen(arg) + 1))
-            return i;
-    return 0;
-}
-
-b8
-evaluate_extension(const str *file_name)
-{
-    u64 len = strlen(file_name);
-    if (len > 2 && strncmp(file_name + len - 2, ".c", 2) == 0)
-        return TRUE;
-    return FALSE;
-}
-
-void
-strip_extension(const str *file_name, str *dest)
-{
-    u64 len = strlen(file_name);
-    str file_name_buf[NAME_MAX] = {0};
-    if (len > 2 && strncmp(file_name + len - 2, ".c", 2) == 0)
-    {
-        snprintf(file_name_buf, len - 1, "%s", file_name);
-        strncpy(dest, file_name_buf, NAME_MAX);
-    }
-}
-
-void
-show_cmd(void)
-{
-    printf("\nCMD:\n");
-    u32 i = 0;
-    for (; i < CMD_MEMB; ++i)
-    {
-        if (!cmd.i[i]) break;
-        printf("    %.3d: %s\n", i, (str*)cmd.i[i]);
-    }
-
-    if (!(flags & FLAG_RAW_CMD))
-        putchar('\n');
-}
-
-void
-raw_cmd(void)
-{
-    printf("\nRAW:\n");
-    u32 i = 0;
-    for (; i < CMD_MEMB; ++i)
-    {
-        if (!cmd.i[i]) break;
-        printf("%s ", (str*)cmd.i[i]);
-    }
-
-    printf("%s", "\n\n");
-}
-
-void
-push_cmd(const str *string)
-{
-    if (cmd_pos >= CMD_MEMB - 1)
-    {
-        LOGERROR(ERR_BUFFER_FULL, "%s\n", "cmd Full");
-        return;
-    }
-
-    if (strlen(string) >= CMD_SIZE - 1)
-    {
-        LOGERROR(ERR_STRING_TOO_LONG, "string '%s' Too Long\n", string);
-        return;
-    }
-
-    LOGTRACE("Pushing String '%s' to cmd.i[%"PRId64"]..\n", string, cmd_pos);
-    strncpy(cmd.i[cmd_pos], string, CMD_SIZE);
-    ++cmd_pos;
-}
-
-void
-build_game(void)
-{
-    snprintf(str_dir_root, CMD_SIZE, "%s%s", str_bin_root, DIR_ROOT);
-    if (mem_alloc_buf(&cmd, CMD_MEMB, CMD_SIZE, "cmd") != ERR_SUCCESS)
-        fail_cmd();
-
-    u32 i = 0;
-    push_cmd(COMPILER);
-    push_cmd(DIR_SRC"main.c");
-    push_cmd(DIR_SRC"chunking.c");
-    push_cmd(DIR_SRC"dir.c");
-    push_cmd(DIR_SRC"gui.c");
-    push_cmd(DIR_SRC"input.c");
-    push_cmd(DIR_SRC"logic.c");
-    push_cmd("-I.");
-    push_cmd("-Wl,-rpath=$ORIGIN/lib/"PLATFORM);
-    push_cmd("-std=c99");
-    push_cmd("-ggdb");
-    push_cmd("-Wall");
-    push_cmd("-Wextra");
-    push_cmd("-fno-builtin");
-    push_cmd("-Wno-implicit-function-declaration");
-    push_cmd("-Llib/"PLATFORM);
-    for (; i < arr_len(str_libs); ++i)
-        push_cmd(str_libs[i]);
-
-    push_cmd("-o");
-    push_cmd(STR_OUT);
-
-#if PLATFORM_LINUX
-    cmd.i[cmd_pos] = NULL;
-#endif
-}
-
-void
-build_engine(void)
-{
-    snprintf(str_dir_root, CMD_SIZE, "%s%s", str_bin_root, DIR_ROOT);
-    if (mem_alloc_buf(&cmd, CMD_MEMB, CMD_SIZE, "cmd") != ERR_SUCCESS)
-        fail_cmd();
-
-    push_cmd(COMPILER);
-    push_cmd("engine/core.c");
-    push_cmd("engine/dir.c");
-    push_cmd("engine/logger.c");
-    push_cmd("engine/math.c");
-    push_cmd("engine/memory.c");
-    push_cmd("engine/platform_"_PLATFORM".c");
-    push_cmd("engine/include/glad/glad.c");
-    push_cmd("-I.");
-    push_cmd("-shared");
-    push_cmd("-fPIC");
-    push_cmd("-std=c99");
-    push_cmd("-ggdb");
-    push_cmd("-Wall");
-    push_cmd("-Wextra");
-    push_cmd("-fno-builtin");
-    push_cmd("-Wno-implicit-function-declaration");
-
-    push_cmd("-o");
-    push_cmd("lib/"_PLATFORM"/"ENGINE_NAME_LIB);
-
-#if PLATFORM_LINUX
-    cmd.i[cmd_pos] = NULL;
-#endif
-}
-
-void
-build_test(char **argv)
-{
-    if (!argv[2])
-    {
-        LOGERROR(ERR_POINTER_NULL, "Usage: ./build%s test [n]\n", EXE);
-        fail_cmd();
-    }
-    u32 i = 0;
-    str temp[CMD_SIZE] = {0};
-    u64 test_index = atoi(argv[2]);
-    str_tests.memb = get_dir_entry_count(DIR_TESTS);
-    if (engine_err != ERR_SUCCESS)
-        fail_cmd();
-    if (test_index >= str_tests.memb)
-    {
-        LOGERROR(ERR_BUFFER_OVERFLOW, "'%s' Invalid, Try './build%s list'"
-                "to List Available Options..\n", argv[2], EXE);
-        fail_cmd();
-    }
-    str_tests = get_dir_contents(DIR_TESTS);
-    if (engine_err != ERR_SUCCESS)
-        fail_cmd();
-    if (mem_alloc_buf(&cmd, CMD_MEMB, CMD_SIZE, "cmd") != ERR_SUCCESS)
-        fail_cmd();
-
-    for (i = 0; i < str_tests.memb; ++i)
-        strip_extension(str_tests.i[i], str_tests.i[i]);
-
-    snprintf(str_dir_root, CMD_SIZE, "%s%s%s/",
-            str_bin_root, DIR_ROOT_TESTS, (str*)str_tests.i[test_index]);
-
-    /* ---- start ----------------------------------------------------------- */
-    push_cmd(COMPILER);
-    push_cmd(stringf("%s%s.c", DIR_TESTS, str_tests.i[test_index]));
-
-    /* ---- children -------------------------------------------------------- */
-    push_cmd("engine/core.c");
-    push_cmd("engine/dir.c");
-    push_cmd("engine/logger.c");
-    push_cmd("engine/math.c");
-    push_cmd("engine/memory.c");
-    push_cmd("engine/platform_"_PLATFORM".c");
-
-    /* ---- includes -------------------------------------------------------- */
-    push_cmd(stringf("-I%s", str_bin_root));
-    snprintf(temp, CMD_SIZE, "%sengine/include/glad/glad.c", str_bin_root);
-    normalize_slash(temp);
-    push_cmd(temp);
-
-    /* ---- cflags ---------------------------------------------------------- */
-    push_cmd(("-Wl,-rpath=$ORIGIN/lib/"PLATFORM));
-    push_cmd("-std=c99");
-    push_cmd("-Wall");
-    push_cmd("-Wextra");
-    push_cmd("-fno-builtin");
-
-    /* ---- libs ------------------------------------------------------------ */
-    snprintf(temp, CMD_SIZE, "-L%slib/"PLATFORM, str_bin_root);
-    push_cmd(temp);
-    for (i = 0; i < arr_len(str_libs); ++i)
-        push_cmd(str_libs[i]);
-
-    /* ---- out ------------------------------------------------------------- */
-    push_cmd("-o");
-    push_cmd(stringf("./%s%s/%s%s", DIR_ROOT_TESTS,
-                str_tests.i[test_index], str_tests.i[test_index], EXE));
-
-#if PLATFORM_LINUX
-    cmd.i[cmd_pos] = NULL;
-#endif
-}
-
-void
-free_cmd(void)
-{
-    mem_free((void*)&str_bin_root, CMD_SIZE, "str_bin_root");
-    mem_free_buf(&str_tests, "str_tests");
-    mem_free_buf(&cmd, "cmd");
-}
-
-void
-fail_cmd(void)
-{
-    free_cmd();
-    _exit(engine_err);
-}
-
-void
-help(void)
-{
-    printf("%s",
-            "Usage: ./build [options]...\n"
-            "Options:\n"
-            "    help       print this help\n"
-            "    list       list all available options and tests\n"
-            "    show       show build command\n"
-            "    raw        show build command, raw\n");
-    exit(EXIT_SUCCESS);
-}
-
-void
-list(void)
-{
-    printf("%s",
-            "Options:\n"
-            "    engine\n"
-            "    launcher\n"
-            "    test [n]\n"
-            "Tests:\n");
-
-    str_tests = get_dir_contents(DIR_TESTS);
-    if (engine_err != ERR_SUCCESS)
-    {
-        LOGERROR(engine_err, "%s\n", "Fetching Tests Failed");
-        fail_cmd();
-    }
-
-    str *str_tests_temp = NULL;
-    if (mem_alloc((void*)&str_tests_temp, NAME_MAX,
-                "str_tests_temp") != ERR_SUCCESS)
-    {
-        LOGERROR(engine_err, "%s\n", "Fetching Tests Failed");
-        fail_cmd();
-    }
-
-    u32 i = 0;
-    for (; i < str_tests.memb; ++i)
-    {
-        if (!evaluate_extension(str_tests.i[i]))
-            continue;
-
-        strip_extension(str_tests.i[i], str_tests_temp);
-        printf("    %03d: %s\n", i, str_tests_temp);
-    }
-
-    mem_free((void*)&str_tests_temp, NAME_MAX, "str_tests_temp");
-    mem_free_buf(&str_tests, "str_tests");
-    engine_err = ERR_SUCCESS;
-    exit(engine_err);
-}
 
 int
 main(int argc, char **argv)
 {
-    log_level_max = LOGLEVEL_INFO;
-    if (compare_argv("LOGFATAL", argc, argv)) log_level_max = LOGLEVEL_FATAL;
-    if (compare_argv("LOGERROR", argc, argv)) log_level_max = LOGLEVEL_ERROR;
-    if (compare_argv("LOGWARN", argc, argv)) log_level_max = LOGLEVEL_WARNING;
-    if (compare_argv("LOGINFO", argc, argv)) log_level_max = LOGLEVEL_INFO;
-    if (compare_argv("LOGDEBUG", argc, argv)) log_level_max = LOGLEVEL_DEBUG;
-    if (compare_argv("LOGTRACE", argc, argv)) log_level_max = LOGLEVEL_TRACE;
+    build_init(argc, argv, "build.c", "build"EXE);
+    if (argv_compare("engine", argc, argv))
+        engine_build(
+                stringf("%sengine/", str_build_root),
+                stringf("%slib/"_PLATFORM, str_build_root)
+                );
 
-    init_build();
+    if (is_dir_exists(DIR_SRC, TRUE) != ERR_SUCCESS)
+        return *BUILD_ERR;
 
-    if (STD != STD_C99)
-    {
-        LOGINFO("%s\n", "Rebuilding Self With -std=c99..");
-        rebuild_self(argv);
-    }
-
-    if (is_source_changed() == ERR_SUCCESS)
-        rebuild_self(argv);
-
-    if (compare_argv("help", argc, argv))       help();
-    if (compare_argv("list", argc, argv))       list();
-    if (compare_argv("test", argc, argv))       state = STATE_TEST;
-    if (compare_argv("engine", argc, argv))     state = STATE_ENGINE;
-    if (compare_argv("show", argc, argv))       flags |= FLAG_SHOW_CMD;
-    if (compare_argv("raw", argc, argv))        flags |= FLAG_RAW_CMD;
-
-    if (
-            is_dir_exists(DIR_SRC, TRUE) != ERR_SUCCESS ||
-            is_dir_exists(DIR_TESTS, TRUE) != ERR_SUCCESS)
-        return engine_err;
-
-    if (!state)                     build_game();
-    else if (state == STATE_TEST)   build_test(argv);
-    else if (state == STATE_ENGINE) build_engine();
-
-    if (flags & FLAG_SHOW_CMD) show_cmd();
-    if (flags & FLAG_RAW_CMD) raw_cmd();
-
-    str str_from[ASSET_COUNT][CMD_SIZE] = {0};
-    str str_to[ASSET_COUNT][CMD_SIZE] = {0};
-    str str_mkdir[ASSET_COUNT][CMD_SIZE] = {0};
-    snprintf(str_from[0],   CMD_SIZE, "%sLICENSE", str_bin_root);
-    snprintf(str_from[1],   CMD_SIZE, "%slib/"PLATFORM, str_bin_root);
-    snprintf(str_from[2],   CMD_SIZE, "%sresources/", str_bin_root);
-    snprintf(str_to[0],     CMD_SIZE, "%sLICENSE", str_dir_root);
-    snprintf(str_to[1],     CMD_SIZE, "%slib/"PLATFORM, str_dir_root);
-    snprintf(str_to[2],     CMD_SIZE, "%sresources/", str_dir_root);
-    snprintf(str_mkdir[0],  CMD_SIZE, "%s", str_dir_root);
-    snprintf(str_mkdir[1],  CMD_SIZE, "%slib/", str_dir_root);
-    snprintf(str_mkdir[2],  CMD_SIZE, "%s", DIR_ROOT_TESTS);
+    snprintf(str_out_dir, CMD_SIZE, "%s%s", str_build_root, DIR_ROOT);
 
     u32 i = 0;
-    for (; i < ASSET_COUNT; ++i)
+    cmd_push(COMPILER);
+    cmd_push(DIR_SRC"main.c");
+    cmd_push(DIR_SRC"chunking.c");
+    cmd_push(DIR_SRC"dir.c");
+    cmd_push(DIR_SRC"gui.c");
+    cmd_push(DIR_SRC"input.c");
+    cmd_push(DIR_SRC"logic.c");
+    cmd_push("-I.");
+    cmd_push("-Wl,-rpath=$ORIGIN/lib/"PLATFORM);
+    cmd_push("-std=c99");
+    cmd_push("-ggdb");
+    cmd_push("-Wall");
+    cmd_push("-Wextra");
+    cmd_push("-fno-builtin");
+    cmd_push("-Llib/"PLATFORM);
+    for (i = 0; i < arr_len(str_libs); ++i)
+        cmd_push(str_libs[i]);
+    cmd_push("-o");
+    cmd_push(STR_OUT);
+    cmd_ready();
+
+    if (flag & FLAG_CMD_SHOW) cmd_show();
+    if (flag & FLAG_CMD_RAW) cmd_raw();
+
+    str str_mkdir[NEW_DIR_COUNT][CMD_SIZE] = {0};
+    str str_from[ASSET_COUNT][CMD_SIZE] = {0};
+    str str_to[ASSET_COUNT][CMD_SIZE] = {0};
+    snprintf(str_mkdir[0],  CMD_SIZE, "%s", str_out_dir);
+    snprintf(str_mkdir[1],  CMD_SIZE, "%slib/", str_out_dir);
+    snprintf(str_from[0],   CMD_SIZE, "%sLICENSE", str_build_root);
+    snprintf(str_from[1],   CMD_SIZE, "%slib/"PLATFORM, str_build_root);
+    snprintf(str_from[2],   CMD_SIZE, "%sresources/", str_build_root);
+    snprintf(str_to[0],     CMD_SIZE, "%sLICENSE", str_out_dir);
+    snprintf(str_to[1],     CMD_SIZE, "%slib/"PLATFORM, str_out_dir);
+    snprintf(str_to[2],     CMD_SIZE, "%sresources/", str_out_dir);
+
+    for (i = 0; i < NEW_DIR_COUNT; ++i)
     {
-        normalize_slash(str_from[i]);
-        normalize_slash(str_to[i]);
         normalize_slash(str_mkdir[i]);
         make_dir(str_mkdir[i]);
     }
 
     for (i = 0; i < ASSET_COUNT; ++i)
     {
+        normalize_slash(str_from[i]);
+        normalize_slash(str_to[i]);
         if (is_file(str_from[i]) == ERR_SUCCESS)
-        {
-            if (copy_file(str_from[i], str_to[i],
-                        "rb", "wb") != ERR_SUCCESS)
-                goto cleanup;
-        }
-        else if (copy_dir(str_from[i], str_to[i],
-                    TRUE, "rb", "wb") != ERR_SUCCESS)
+            copy_file(str_from[i], str_to[i], "rb", "wb");
+        else copy_dir(str_from[i], str_to[i], TRUE, "rb", "wb");
+
+        if (*BUILD_ERR != ERR_SUCCESS)
             goto cleanup;
     }
 
     if (exec(&cmd, "build") != ERR_SUCCESS)
         goto cleanup;
 
-    free_cmd();
-    engine_err = ERR_SUCCESS;
-    return engine_err;
+    cmd_free();
+    *BUILD_ERR = ERR_SUCCESS;
+    return *BUILD_ERR;
 
 cleanup:
-    fail_cmd();
-    return engine_err;
+    cmd_fail();
+    return *BUILD_ERR;
 }
---
+++ engine/build.c
@@ -0,0 +1,300 @@
+#include <unistd.h>
+
+#include "h/build.h"
+#include "memory.c"
+#include "dir.c"
+#include "logger.c"
+
+#if PLATFORM_LINUX
+    #include "platform_linux.c"
+#elif PLATFORM_WIN
+    #include "platform_windows.c"
+#endif /* PLATFORM */
+
+#if defined(__STDC_VERSION__)
+    #define STD __STDC_VERSION__
+#elif defined(__STDC__)
+    #define STD 89
+#endif /* STD */
+
+#define STD_C99 199901L
+
+u32 engine_err = ERR_SUCCESS;
+static u32 flag = 0;
+str *str_build_root = NULL;
+static str str_build_src[CMD_SIZE] = {0};
+static str str_build_bin[CMD_SIZE] = {0};
+static str str_build_bin_new[CMD_SIZE] = {0};
+static str str_build_bin_old[CMD_SIZE] = {0};
+buf cmd = {NULL};
+static u64 cmd_pos = 0;
+
+static u32 is_build_source_changed(void);
+
+/* can force-terminate process */
+static void self_rebuild(char **argv);
+
+static void cmd_show(void);
+static void cmd_raw(void);
+static void help(void);
+
+void
+build_init(int argc, char **argv,
+        const str *build_src_name, const str *build_bin_name)
+{
+    if (argv_compare("help", argc, argv))       help();
+    if (argv_compare("show", argc, argv))       flag |= FLAG_CMD_SHOW;
+    if (argv_compare("raw", argc, argv))        flag |= FLAG_CMD_RAW;
+
+    log_level_max = LOGLEVEL_INFO;
+    if (argv_compare("LOGFATAL", argc, argv)) log_level_max = LOGLEVEL_FATAL;
+    if (argv_compare("LOGERROR", argc, argv)) log_level_max = LOGLEVEL_ERROR;
+    if (argv_compare("LOGWARN", argc, argv)) log_level_max = LOGLEVEL_WARNING;
+    if (argv_compare("LOGINFO", argc, argv)) log_level_max = LOGLEVEL_INFO;
+    if (argv_compare("LOGDEBUG", argc, argv)) log_level_max = LOGLEVEL_DEBUG;
+    if (argv_compare("LOGTRACE", argc, argv)) log_level_max = LOGLEVEL_TRACE;
+
+    str_build_root = get_path_bin_root();
+    if (engine_err != ERR_SUCCESS)
+        cmd_fail();
+
+    check_slash(str_build_root);
+    normalize_slash(str_build_root);
+
+    snprintf(str_build_src, CMD_SIZE, "%s%s", str_build_root, build_src_name);
+    normalize_slash(str_build_src);
+
+    snprintf(str_build_bin, CMD_SIZE, "%s%s",
+            str_build_root, build_bin_name);
+    snprintf(str_build_bin_new, CMD_SIZE, "%s%s_new",
+            str_build_root, build_bin_name);
+    snprintf(str_build_bin_old, CMD_SIZE, "%s%s_old",
+            str_build_root, build_bin_name);
+
+    if (STD != STD_C99)
+    {
+        LOGINFO("%s\n", "Rebuilding Self With -std=c99..");
+        self_rebuild(argv);
+    }
+
+    if (is_build_source_changed() == ERR_SUCCESS)
+    {
+        LOGINFO("%s\n", "Rebuilding Self..");
+        self_rebuild(argv);
+    }
+
+    if (mem_alloc_buf(&cmd, CMD_MEMB, CMD_SIZE, "cmd") != ERR_SUCCESS)
+        cmd_fail();
+}
+
+static u32
+is_build_source_changed(void)
+{
+    unsigned long mtime_src = 0;
+    unsigned long mtime_bin = 0;
+
+    struct stat stats;
+
+    if (stat(str_build_src, &stats) == 0)
+        mtime_src = stats.st_mtime;
+    else
+    {
+        LOGERROR(ERR_FILE_NOT_FOUND, "%s\n", "Build Source File Not Found");
+        return engine_err;
+    }
+
+    if (stat(str_build_bin, &stats) == 0)
+        mtime_bin = stats.st_mtime;
+    else
+    {
+        LOGERROR(ERR_FILE_NOT_FOUND, "File 'build%s' Not Found\n", EXE);
+        return engine_err;
+    }
+
+    if (mtime_src && mtime_bin && mtime_src > mtime_bin)
+        return ERR_SUCCESS;
+
+    engine_err = ERR_SOURCE_NOT_CHANGE;
+    return engine_err;
+}
+
+static void
+self_rebuild(char **argv)
+{
+    if (mem_alloc_buf(&cmd, 16, CMD_SIZE, "cmd") != ERR_SUCCESS)
+        cmd_fail();
+
+    u32 i = 0;
+    cmd_push(COMPILER);
+    cmd_push(stringf("%s", str_build_src));
+    cmd_push("-ggdb");
+    cmd_push("-std=c99");
+    cmd_push("-Wall");
+    cmd_push("-Wextra");
+    cmd_push("-fno-builtin");
+    cmd_push("-Wno-implicit-function-declaration");
+    cmd_push("-o");
+    cmd_push(str_build_bin_new);
+    cmd_ready();
+
+    if (flag & FLAG_CMD_SHOW) cmd_show();
+    if (flag & FLAG_CMD_RAW) cmd_raw();
+
+    if (exec(&cmd, "cmd") == ERR_SUCCESS)
+    {
+        LOGINFO("%s\n", "Self Rebuild Success");
+        rename(str_build_bin, str_build_bin_old);
+        rename(str_build_bin_new, str_build_bin);
+        remove(str_build_bin_old);
+
+        execvp(argv[0], (str *const *)argv);
+        LOGFATAL(engine_err, "'build%s' Failed, Process Aborted\n", EXE);
+        cmd_fail();
+    }
+
+    LOGFATAL(engine_err, "%s\n", "Self-Rebuild Failed, Process Aborted");
+    cmd_fail();
+}
+
+u32
+engine_build(const str *engine_dir, const str *out_dir)
+{
+    if (
+            is_dir_exists(engine_dir, TRUE) != ERR_SUCCESS ||
+            is_dir_exists(out_dir, TRUE) != ERR_SUCCESS)
+        return engine_err;
+
+    str engine_dir_processed[CMD_SIZE] = {0};
+    str out_dir_processed[CMD_SIZE] = {0};
+    memcpy(engine_dir_processed, engine_dir, CMD_SIZE);
+    memcpy(out_dir_processed, out_dir, CMD_SIZE);
+
+    check_slash(engine_dir_processed);
+    check_slash(out_dir_processed);
+    cmd_push(COMPILER);
+    cmd_push(stringf("%score.c", engine_dir_processed));
+    cmd_push(stringf("%sdir.c", engine_dir_processed));
+    cmd_push(stringf("%slogger.c", engine_dir_processed));
+    cmd_push(stringf("%smath.c", engine_dir_processed));
+    cmd_push(stringf("%smemory.c", engine_dir_processed));
+    cmd_push(stringf("%splatform_"_PLATFORM".c", engine_dir_processed));
+    cmd_push(stringf("%sinclude/glad/glad.c", engine_dir_processed));
+    cmd_push("-I.");
+    cmd_push("-shared");
+    cmd_push("-fPIC");
+    cmd_push("-std=c99");
+    cmd_push("-Wall");
+    cmd_push("-Wextra");
+    cmd_push("-fno-builtin");
+    cmd_push("-Wno-implicit-function-declaration");
+    cmd_push("-o");
+    cmd_push(stringf("%s"ENGINE_NAME_LIB, out_dir_processed));
+    cmd_ready();
+
+    if (flag & FLAG_CMD_SHOW) cmd_show();
+    if (flag & FLAG_CMD_RAW) cmd_raw();
+
+    if (exec(&cmd, "build") != ERR_SUCCESS)
+        cmd_fail();
+
+    cmd_free();
+    engine_err = ERR_SUCCESS;
+    _exit(engine_err);
+    return engine_err;
+}
+
+u64
+argv_compare(str *arg, int argc, char **argv)
+{
+    if (argc == 1)
+        return 0;
+
+    u32 i = 1;
+    for (; (int)i < argc; ++i)
+        if (!strncmp(argv[i], arg, strlen(arg) + 1))
+            return i;
+    return 0;
+}
+
+static void
+cmd_show(void)
+{
+    printf("\nCMD:\n");
+    u32 i = 0;
+    for (; i < CMD_MEMB; ++i)
+    {
+        if (!cmd.i[i]) break;
+        printf("    %.3d: %s\n", i, (str*)cmd.i[i]);
+    }
+
+    if (!(flag & FLAG_CMD_RAW))
+        putchar('\n');
+}
+
+static void
+cmd_raw(void)
+{
+    printf("\nRAW:\n");
+    u32 i = 0;
+    for (; i < CMD_MEMB; ++i)
+    {
+        if (!cmd.i[i]) break;
+        printf("%s ", (str*)cmd.i[i]);
+    }
+
+    printf("%s", "\n\n");
+}
+
+void
+cmd_push(const str *string)
+{
+    if (cmd_pos >= CMD_MEMB - 1)
+    {
+        LOGERROR(ERR_BUFFER_FULL, "%s\n", "cmd Full");
+        return;
+    }
+
+    if (strlen(string) >= CMD_SIZE - 1)
+    {
+        LOGERROR(ERR_STRING_TOO_LONG, "string '%s' Too Long\n", string);
+        return;
+    }
+
+    LOGTRACE("Pushing String '%s' to cmd.i[%"PRId64"]..\n", string, cmd_pos);
+    strncpy(cmd.i[cmd_pos++], string, CMD_SIZE);
+}
+
+void
+cmd_ready(void)
+{
+#if PLATFORM_LINUX
+    cmd.i[cmd_pos] = NULL;
+#endif
+}
+
+void
+cmd_free(void)
+{
+    mem_free((void*)&str_build_root, CMD_SIZE, "str_build_root");
+    mem_free_buf(&cmd, "cmd");
+    cmd_pos = 0;
+}
+
+void
+cmd_fail(void)
+{
+    cmd_free();
+    _exit(engine_err);
+}
+
+static void
+help(void)
+{
+    printf("%s",
+            "Usage: ./build [options]...\n"
+            "Options:\n"
+            "    help       print this help\n"
+            "    show       show build command\n"
+            "    raw        show build command, raw\n");
+    exit(ERR_SUCCESS);
+}
